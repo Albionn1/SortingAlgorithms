dcde7e65924deac7c165618330dc323b91627618
Albion Berisha <57451316+Albionn1@users.noreply.github.com>
2025-11-20 02:59:07 +0100

    Delete build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug directory
    Deleted the build folder

--- build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_SortingAlgorithms_FILE C:/Users/Albio/Documents/C++/FUNTHINGS/FORFUN/SortingAlgosGUI/SortingAlgorithms/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/SortingAlgorithms.exe)
-set(__QT_DEPLOY_TARGET_SortingAlgorithms_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_SortingAlgorithms_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
